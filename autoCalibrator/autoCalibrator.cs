bf55bb020aa438771db0e37b1ccad0e8277b1337
Dez <dez@lookingglassfactory.com>
2016-10-20 01:38:54 -0400

    added method for picking out peaks out of the data

--- autoCalibrator/autoCalibrator.cs
+++ autoCalibrator/autoCalibrator.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 
 //this autocalibrator is a state machine
@@ -133,6 +134,125 @@ namespace hypercube
             gradientLine.transform.localPosition = new Vector3((2f * posX * pixelX * aspectRatio) - ( pixelX * screenX *  aspectRatio), 1f-(posY * pixelY * 2f), 0f);
 
         }
-    }
 
+        public Vector3 screenCoordToLocalCoord(float x, float y)
+        {
+            float screenX = 0f;
+            float screenY = 0f;
+            canvas.getScreenDims(ref screenX, ref screenY);
+            float pixelX = 1f / screenX;
+            float pixelY = 1f / screenY;
+
+            float aspectRatio = canvas.getScreenAspectRatio();
+            return new Vector3((2f * x * pixelX * aspectRatio) - (pixelX * screenX * aspectRatio), 1f - (y * pixelY * 2f), 0f);
+        }
+
+        /// <summary>
+        /// This method will extract 'peaks' from noisy data, returning the positions in the array where it thinks the peaks occurred.
+        /// Note that the returned values are floats: It uses averages to figure out where it thinks the true peak is which may lie 'in between' the given data points.
+        /// </summary>
+        /// <param name="minPeakAmplitude">The minimum 'height' a peak should be.</param>
+        /// <param name="minPeakFreq">The minimum 'width' of expected peaks. This will keep it from returning noise as peaks.</param>
+        /// <param name="data">The data to obtain the peaks from.</param>
+        /// <returns></returns>
+        public static float[] getPeaksFromData(int minPeakAmplitude, int minPeakFreq, int[] data)
+        {
+            //first, we will filter out what parts of the data count as a part of a peak.
+            int halfFreq = Mathf.RoundToInt((float)minPeakFreq/2f);
+            int first = 0;
+            int last = 0;
+            List<int> peakData = new List<int>(); //store here any values that meet basic peak criteria
+            for (int i = 0; i < data.Length; i++)
+            {
+                first = i - halfFreq;
+                last = i + halfFreq;
+
+                if (first < 0 && last >= data.Length)
+                    Debug.LogError("Data set is too small to analyze using a minPeakFreq of " + minPeakFreq);
+
+                if (first < 0) //near start of analysis
+                {
+                    if (checkPeak(0f, data[i], data[last], minPeakAmplitude))
+                        peakData.Add(i);
+                }
+                else if (last >= data.Length) //near end of analysis
+                {
+                    if (checkPeak(data[first], data[i], 0f, minPeakAmplitude))
+                        peakData.Add(i);
+                }
+                else //normal path
+                {
+                    if (checkPeak(data[first], data[i], data[last], minPeakAmplitude))
+                        peakData.Add(i);
+                }
+            }
+
+            if (peakData.Count == 0) //sanity check
+            {
+                Debug.LogWarning("Could not find any peaks in the given data. Is the sensor broken? Printing raw data to log...");
+                Debug.Log(data);
+                return new float[0]; //could not find peaks
+            }
+                
+
+            //now we know what parts of the data are part of peaks. Let's arrange them into groups (peaks).
+            //maxAllowedMissingDataPoints will be used to separate out the peaks from each other.
+            List<List<int>> noisyPeaks = new List<List<int>>();    
+            int maxAllowedMissingDataPoints =  Mathf.RoundToInt((float)minPeakFreq / 10f);
+            if (maxAllowedMissingDataPoints < 3)
+                maxAllowedMissingDataPoints = 3;
+            int currentPeak = 0;
+            noisyPeaks.Add(new List<int>());
+            for (int d = 0; d < peakData.Count -1; d++)
+            {
+                if (peakData[d + 1] - peakData[d] < maxAllowedMissingDataPoints)
+                    noisyPeaks[currentPeak].Add(d);
+                else
+                {
+                    noisyPeaks.Add(new List<int>());
+                    currentPeak++;
+                }
+            }
+
+            //lets, ignore any peaks with very few data points (which are probably mostly noise)
+            int thirdFreq = Mathf.RoundToInt((float)minPeakFreq / 3f);
+            List<List<int>> peaks = new List<List<int>>();
+            for (int p = 0; p < noisyPeaks.Count; p++)
+            {
+                if (noisyPeaks[p].Count >= thirdFreq)
+                    peaks.Add(noisyPeaks[p]);
+            }
+
+
+            //now lets do a biased average of the data contained in each peak, and return that as our final values.
+            List<float> output = new List<float>();
+            foreach (List<int> p in peaks)
+            {
+                output.Add(findPeakInDataPoints(p, data, .8f));
+            }
+
+            return output.ToArray();
+        }
+
+        static bool checkPeak(int first, int middle, int last, int threshold)
+        {
+            if (middle - first > threshold && middle - last > threshold)
+                return true;
+            return false;
+        }
+
+        // use a weighted average to find the peak
+        static float findPeakInDataPoints(List<int> dataPoints, int[] allData, float bias)
+        {
+            ulong total = 0;
+            ulong div = 0;
+            foreach (int d in dataPoints)
+            {
+                total += (ulong)(d * allData[d]);
+                div += (ulong)allData[d];
+            }
+            return (float)(total/div);
+        }
+
+    }
 }
